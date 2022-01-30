62b7bc25f05d71d0e867ac9e83ecf33a8d8f0a5a
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2022-01-30 17:30:50 +0600

    Initial commit

---
+++ script.py
@@ -0,0 +1,21 @@
+output = open("output.txt", "w", encoding="utf-8")
+
+with open("./BengaliWordList_439.txt", encoding="utf-8") as f:
+    for line in f.readlines():
+        if not any(
+            i in line
+            for i in (
+                "া",  # আ-কার
+                "ি",  # ই-কার
+                "ী",  # ঈ-কার
+                "ু",  # উ-কার
+                "ূ",  # ঊ-কার
+                "ৃ",  # ঋ-কার
+                "ে",  # এ-কার
+                "ৈ",  # ঐ-কার
+                "ো",  # ও-কার
+                "ৌ",  # ঔ-কার
+            )
+        ):
+            output.write(line)
+output.close()
